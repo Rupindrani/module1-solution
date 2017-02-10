0076ef95c7c4fa18b119a9fe5acc4891e2435e9b
Rupindrani <rupindrani@example.com>
2017-02-09 22:21:29 -0500

    module1-solution

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 (function() {
   'use strict';
-  angular.module ('lunchCheckApp',[])
-  .controler('myLunchCheckController', function(){
-
-  });
+  angular.module ('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject =["$scope"];
+  function LunchCheckController($scope) {
+    $scope.listOfItem = "";
+    $scope.checkItems = function() {
+      if ($scope.listOfItem.length >0) {
+        var strArray = $scope.listOfItem.split(",");
+        if(strArray.length <=3) {
+          $scope.result="Enjoy!";
+        }
+        else {
+          $scope.result="Too Much!";
+        }
+      }
+      else{
+        $scope.result="Please enter data first";
+      }
+    }
+  };
 })();
